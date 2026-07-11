f77abce86d3d046e8be23944a60ebbbc8bfbb12d
catacohome <catacohome@gmail.com>
2026-07-11 19:18:23 +1200

    Update void_fluid.js

--- void_fluid.js
+++ void_fluid.js
@@ -1,29 +1,7 @@
-elements.void_fluid = {
-    color: "#5a00ff",
-    behavior: behaviors.LIQUID,
+console.log("VOID FLUID MOD LOADED");
+
+elements.test_block = {
+    color: "#ff0000",
     category: "special",
-    state: "liquid",
-    density: 2000,
-
-    tick(pixel) {
-        for (let dx = -1; dx <= 1; dx++) {
-            for (let dy = -1; dy <= 1; dy++) {
-
-                if (dx === 0 && dy === 0) continue;
-
-                let x = pixel.x + dx;
-                let y = pixel.y + dy;
-
-                if (outOfBounds(x,y)) continue;
-
-                let other = pixelMap[x][y];
-
-                if (other && other.element !== "void_fluid") {
-                    deletePixel(x,y);
-                }
-            }
-        }
-
-        doDefaults(pixel);
-    }
+    behavior: behaviors.WALL,
 };
